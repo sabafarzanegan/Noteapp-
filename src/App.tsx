3b50364f6a90b48dfb1f328558578c74538febb4
saba farzanegan <saba.far.99@gmail.com>
2024-07-05 17:40:34 +0330

    create basic structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-casabella"> 12345سلام</h1>
+      <Button color="warning" size="3xl" rounded="3xl">
+        ارسال
+      </Button>
+    </>
+  );
+}
+
+export default App;
